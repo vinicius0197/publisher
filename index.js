09f5aa6425c52c775458883f47a92e6543b5f35b
Vinicius Costa <viniciuscosta0197@gmail.com>
2020-07-16 23:13:45 -0300

    Added basic scripts for uploading to netlify

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 var showdown = require("showdown");
+const { spawn } = require("child_process");
+const { onExit } = require("@rauschma/stringio");
 var fs = require("fs");
 
-/**
- * 1) should open a folder with markdown files and create an object with keys being names of folders
- *    and contents being the converted HTML content
- * 2) create HTML template with sample CSS styling
- * 3) Add searchbox for searching markdown
- * 4) Deploy script to upload easily
- */
-fs.readFile("markdown/test.md", "utf8", function (err, data) {
-  if (err) throw err;
-  converter = new showdown.Converter();
-  html = converter.makeHtml(data);
-  console.log(html);
-});
+const BUILD_PATH = "./build";
+
+function convertToHtml() {
+  fs.readFile("markdown/test.md", "utf8", function (err, data) {
+    if (err) throw err;
+    converter = new showdown.Converter();
+    html = converter.makeHtml(data);
+    // Creates build directory if it does not exist yet
+    if (!fs.existsSync(BUILD_PATH)) {
+      fs.mkdirSync(BUILD_PATH);
+    }
+    fs.writeFile(`${BUILD_PATH}/index.html`, html, function (err) {
+      if (err) throw err;
+      console.log("Build completed");
+    });
+  });
+}
+
+async function main() {
+  convertToHtml();
+  const netlify = spawn("npm", ["run", "netlify"], {
+    stdio: [process.stdin, process.stdout, process.stderr],
+  });
+
+  await onExit(netlify);
+
+  const netlifyProd = spawn("npm", ["run", "netlify-prod"], {
+    stdio: [process.stdin, process.stdout, process.stderr],
+  });
+
+  await onExit(netlifyProd);
+
+  console.log("Done");
+}
+
+main();
--- markdown/test.md
+++ markdown/test.md
@@ -1,4 +1,4 @@
-# This is a test file
+# Test file
 
 Some test content
 
